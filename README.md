bdbb18a4e98f19374d5128bbf210c89b73184451
sohail07860 <07mohammedsohail@gmail.com>
2023-04-15 11:19:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# reactEventManagement
+ 
---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import events from "./csvjson.json";
+import { Card, CardHeader, CardBody, CardFooter, Box } from "@chakra-ui/react";
+import { Heading, SimpleGrid } from "@chakra-ui/react";
+import { Center, Square, Circle } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box bgColor={"#161625"}>
+      <Center>
+        <Heading as="h1" size="2xl" color={"blue.500"} paddingY={"10"}>
+          Events
+        </Heading>
+      </Center>
+      <div>
+        <SimpleGrid columns={3} spacing={5} minChildWidth="400px" padding={"4"}>
+          {events.map((event) =>
+            event.Title ? (
+              <Card color={"white"} bgColor={"blackAlpha.700"}>
+                <CardHeader fontWeight={"extrabold"} fontSize={"2xl"}>
+                  {event.Title}
+                </CardHeader>
+                <CardBody color={"white"}>{event.Description}</CardBody>
+                <CardBody color={"white"}>{event.Objective}</CardBody>
+                <CardFooter color={"blue.700"}>{event["Organized By"]}</CardFooter>
+              </Card>
+            ) : null
+          )}
+        </SimpleGrid>
+      </div>
+    </Box>
+  );
+}
+
+export default App;
